a5e3dbf5025e21fdcc5c539b1cbb08c27f349138
jbjw <github@jbjw.com>
2017-06-22 18:20:54 -0700

    "sync"

--- webpack.config.js
+++ webpack.config.js
@@ -6,106 +6,102 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/main.jsx',
-  },
+	entry: {
+		app: './src/main.jsx',
+	},
 	devtool: 'source-map',
 	devServer: {
 		inline: true,
 		// hot: true,
 		contentBase: 'dist/',
+		// host: '0.0.0.0',
 		port: 80,
+		// historyApiFallback: true,
+
+		// stats: 'errors-only',
+		// noInfo: true,
+		// watchOptions: {
+		//     aggregateTimeout: 300,
+		//     poll: 1000,
+		//     ignored: /node_modules/
+		// }
 	},
-  // devServer: {
-  //   historyApiFallback: true,
-  //   host: '0.0.0.0',
-  //   port: 3000,
-  //   stats: 'errors-only',
-  //   noInfo: true,
-  //   watchOptions: {
-  //     aggregateTimeout: 300,
-  //     poll: 1000,
-  //     ignored: /node_modules/
-  //   }
-  // },
 
-  output: {
-    path: dist,
-    // filename: '[name].bundle.js',
+	output: {
+		path: dist,
+		// filename: '[name].bundle.js',
 		filename: 'bundle.js',
-  },
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: [src],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        options: {
-          configFile: './.eslintrc',
-        },
-        include: [src],
-        exclude: /node_modules/,
-      },
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				include: [src],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.jsx?$/,
+				enforce: 'pre',
+				loader: 'eslint-loader',
+				options: {
+					configFile: './.eslintrc',
+				},
+				include: [src],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+		]
+	},
 
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-    ]
-  },
+	plugins: [
+		// new webpack.LoaderOptionsPlugin({
+		//   minimize: true,
+		//   debug: false,
+		//   quiet: true
+		// }),
 
-  plugins: [
-    // new webpack.LoaderOptionsPlugin({
-    //   minimize: true,
-    //   debug: false,
-    //   quiet: true
-    // }),
+		// new webpack.optimize.CommonsChunkPlugin({
+		//   name: 'vendor',
+		//   minChunks: 2,
+		//   filename: 'vendor.bundle.js'
+		// }),
 
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   minChunks: 2,
-    //   filename: 'vendor.bundle.js'
-    // }),
+		// new webpack.optimize.UglifyJsPlugin({
+		//   compress: {
+		//     warnings: false,
+		//     screw_ie8: true,
+		//     unused: true,
+		//     dead_code: true,
+		//   },
+		//   output: {
+		//     comments: false
+		//   },
+		//   sourceMap: false
+		// }),
 
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     screw_ie8: true,
-    //     unused: true,
-    //     dead_code: true,
-    //   },
-    //   output: {
-    //     comments: false
-    //   },
-    //   sourceMap: false
-    // }),
+		new CopyWebpackPlugin([
+			{
+				from: 'src/index.html',
+				to: 'index.html'
+			}
+		]),
 
-    new CopyWebpackPlugin([
-      {
-        from: 'src/index.html',
-        to: 'index.html'
-      },
-    ]),
+		new OpenBrowserPlugin({
+			url: 'http://localhost:80/'
+		}),
 
-    new OpenBrowserPlugin({
-      url: 'http://localhost:80/'
-    }),
-
-    new webpack.ProvidePlugin({
-      // 'THREE': 'three',
-      // 'TWEEN': 'tween.js',
-    }),
-  ]
-
+		new webpack.ProvidePlugin({
+			// 'THREE': 'three',
+			// 'TWEEN': 'tween.js',
+		}),
+	]
 }
